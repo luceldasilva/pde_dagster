1d9dcd2b4653e1d6edddf23192306f7f700ba674
Lucel Da Silva <luceldasilva@yahoo.com>
2023-08-25 15:42:43 -0400

    para hacer stream de a 500 registros en leccion 32 Streamed Data Processing

---
+++ pde_dagster/jobs/J4_streamed_process_records.py
@@ -0,0 +1,19 @@
+from dagster import job, in_process_executor
+from pde_dagster.resources.postgres import postgres_resource
+from pde_dagster.ops.J4_streamed_process_records.streamed_process_records import (
+streamed_process_records_op
+)
+
+@job(
+    config={
+        "ops": {
+            "streamed_process_records_op": {
+                "config": {"query_str": "SELECT * FROM pos_transactions"}
+            }
+        }
+    },
+    resource_defs={"postgres_resource": postgres_resource},
+    executor_def=in_process_executor,
+)
+def J4_streamed_process_records():
+    streamed_process_records_op()
--- pde_dagster/repository.py
+++ pde_dagster/repository.py
@@ -6,6 +6,9 @@ from pde_dagster.jobs.J2_partitioned_process_pos_records import (
 from pde_dagster.jobs.J3_partitioned_process_records import (
     J3_partitioned_process_records
 )
+from pde_dagster.jobs.J4_streamed_process_records import (
+    J4_streamed_process_records
+)
 
 
 @repository
@@ -13,7 +16,8 @@ def pde_dagster():
     jobs = [
         J1_process_pos_records,
         J2_partitioned_process_pos_records,
-        J3_partitioned_process_records
+        J3_partitioned_process_records,
+        J4_streamed_process_records
     ]
     schedules = []
     sensors = []
